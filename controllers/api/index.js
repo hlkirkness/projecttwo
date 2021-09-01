545613e63bfdee399113f3ce0dd3376182ac1050
hlkirkness <hlkirkness@gmail.com>
2021-08-31 22:05:27 -0400

    post working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,7 @@ const restaurantRoutes = require('./restaurant-routes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
-router.use('/posts', restaurantRoutes);
+router.use('/restaurants', restaurantRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
